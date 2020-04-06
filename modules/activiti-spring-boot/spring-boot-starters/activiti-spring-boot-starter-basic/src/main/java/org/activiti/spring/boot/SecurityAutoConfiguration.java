21223d93430988fef338b0893e7dbe5eca9954b0
junjun <576583158@qq.com>
2020-04-06 23:31:39 +0800

    兼容 springboot-2.x

--- modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/SecurityAutoConfiguration.java
+++ modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/SecurityAutoConfiguration.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * @author Josh Long
  */
 @Configuration
-@AutoConfigureBefore(org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
+@AutoConfigureBefore(org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class SecurityAutoConfiguration {
 
   @Configuration
@@ -56,7 +56,7 @@ public class SecurityAutoConfiguration {
     @Autowired
     private IdentityService identityService;
   }
-  
+
   @Configuration
   @ConditionalOnClass(name = {"org.activiti.rest.service.api.RestUrls", "org.springframework.web.servlet.DispatcherServlet"})
   @EnableWebSecurity
--- modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
+++ modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,9 @@ import org.activiti.engine.impl.persistence.deploy.DeploymentCache;
 import org.activiti.engine.impl.persistence.deploy.ProcessDefinitionCacheEntry;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.repository.ProcessDefinition;
-import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+//import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.*;
@@ -30,18 +32,21 @@ import java.util.*;
  *
  * @author Josh Long
  */
-@ConfigurationProperties(prefix = "endpoints.activiti")
-public class ProcessEngineEndpoint extends AbstractEndpoint<Map<String, Object>> {
+//@ConfigurationProperties(prefix = "endpoints.activiti")
+@Endpoint(id = "activiti")
+public class ProcessEngineEndpoint {
+//public class ProcessEngineEndpoint extends AbstractEndpoint<Map<String, Object>> {
 
     private final ProcessEngine processEngine;
 
     public ProcessEngineEndpoint(ProcessEngine processEngine) {
-        super("activiti");
+//        super("activiti");
         this.processEngine = processEngine;
     }
 
-    @Override
-    public Map<String, Object> invoke() {
+//    @Override
+    @ReadOperation
+    public Map<String, Object> activiti() {
 
         Map<String, Object> metrics = new HashMap<String, Object>();
 
